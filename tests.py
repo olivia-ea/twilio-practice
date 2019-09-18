987aa68b613c65be9ef0b65fce894d12ff9ce487
Olivia Ea <ea.olivia1@gmail.com>
2019-09-17 21:13:36 -0700

    Fixed os vars and added testing

--- tests.py
+++ tests.py
@@ -1,15 +1,90 @@
 import unittest
-import unittest.mock
+from nose.tools import assert_is_not_none
+from unittest.mock import Mock, patch
+from views import *
+from constants import *
+import requests
+import json
+from twilio.base.exceptions import TwilioRestException
+
+
+
+# nosetests --verbosity=2 tests.py
 
 class TestTwilioMicroservice(unittest.TestCase):
-    def test_valid_response_for_create_sms_function(self):
-        # check if phone number is valid
-        # check if message body is not empty
-        # check status code is not 200 => response will be none
-        # if status code is 200 => check if it's in dict format
+    # @classmethod
+    # def setup_class(cls):
+    #     # cls.mock_get_patcher = patch('views.requests.get')
+    #     # cls.mock_get = cls.mock_get_patcher.start()
+    #     self.app = app.test_client()
+    #     self.app.testing = True
+    #
+    # @classmethod
+    # def teardown_class(cls):
+    #     cls.mock_get_patcher.stop()
+    def setUp(self):
+        self.app = app.test_client()
+
+    def tearDown(self):
+        pass
+
+    # def test_valid_send_message_action(self):
+    #     info = {'message_body': 'Testing Twilio post request', 'phone_number': '+15107897844'}
+    #     response = self.app.post('/messages',
+    #                              headers={'Content-Type': 'application/json'},
+    #                              data=json.dumps(info))
+    #     encoded_data = response.data.decode("utf-8")
+    #     data = json.loads(encoded_data)
+    #     self.assertIn('Testing Twilio post request', data['message_body'])
+    #     self.assertIn('15106171282', data['message_from'])
+    #     self.assertEqual(response.status_code, 200)
+
+    def test_invalid_send_message_action(self):
+        info = {'message_body': 'Testing Twilio post request', 'phone_number': '+99999999999999999'}
+        response = self.app.post('/messages',
+                                 headers={'Content-Type': 'application/json'},
+                                 data=json.dumps(info))
+        self.assertEqual(response.status_code, 500)
+        self.assertFalse(is_valid_phone_number(info['phone_number']))
+
+    def test_valid_view_message_by_id(self):
+        pass
+
+    def test_invalid_view_message_by_id(self):
+        pass
+
+    def test_valid_view_all_messages(self):
+        result = self.app.get('/filter_by')
+        encoded_data = result.data.decode("utf-8")
+        data = json.loads(encoded_data)
+        self.assertEqual(result.status_code, 200)
+        self.assertIn('Sent from your Twilio trial account', data['message_body'])
+        self.assertIn('15106171282', data['message_sent_from'])
+
+    def test_invalid_view_all_messages(self):
+        pass
+
+    def test_valid_filter_messages_by_sent_to(self):
+        pass
+
+    def test_invalid_filter_messages_by_sent_to(self):
+        pass
+
+    def test_valid_phone_number(self):
+        pass
+
+    def test_invalid_phone_number(self):
+        pass
+
+    def test_valid_filter_messages_by_from(self):
+        pass
+
+    def test_invalid_filter_messages_by_from(self):
+        pass
 
 
-# TODO look up mocking and stubbing
-
+# TODO write test to make sure user is putting in correct input for phone number
+# Error: test_client not working as expected; post request for testing still sending to phone number.
+# Error:
 if __name__ == '__main__':
     unittest.main()
--- views.py
+++ views.py
@@ -1,29 +1,39 @@
 from flask import Flask, request
-from twilio.rest import Client
 from constants import *
+from twilio.base.exceptions import TwilioRestException
 
 app = Flask(__name__)
 
+def is_valid_phone_number(phone_number):
+    response = CLIENT.lookups.phone_numbers(phone_number).fetch()
+    if response.status_code == 200:
+        return True
+    else:
+        return False
+
 @app.route('/messages', methods=['POST'])
-def send_message():
-    client = Client(TWILIO_ACCOUNT_SID, TWILIO_AUTH_TOKEN)
+def send_message_action():
     request_body = request.get_json()
-    message = client.messages.create(
-                              body=request_body['message_body'],
-                              from_=TWILIO_PHONE_NUMBER,
-                              to=request_body['phone_number']
-                              )
-    message_dict = {}
-    message_dict['message_sid'] = message.sid
-    message_dict['message_to'] = message.to
-    message_dict['message_from'] = message.from_
-    message_dict['message_body'] = message.body
-    return message_dict
+    phone_number = request_body['phone_number']
+    if is_valid_phone_number(phone_number):
+        message = CLIENT.messages.create(
+                                         body=request_body['message_body'],
+                                         from_=TWILIO_PHONE_NUMBER,
+                                         to=request_body['phone_number']
+                                         )
+        message_dict = {}
+        message_dict['message_sid'] = message.sid
+        message_dict['message_to'] = message.to
+        message_dict['message_from'] = message.from_
+        message_dict['message_body'] = message.body
+        return message_dict
+    else:
+        # raise ValueError('Not a valid phone number!')
+        return 404
 
 @app.route('/messages/<message_id>', methods=['GET'])
-def read_message(message_id):
-    client = Client(TWILIO_ACCOUNT_SID, TWILIO_AUTH_TOKEN)
-    message = client.messages(message_id).fetch()
+def view_message_by_id(message_id):
+    message = CLIENT.messages(message_id).fetch()
     message_dict = {}
     message_dict['account_sid'] = message.account_sid
     message_dict['api_version'] = message.api_version
@@ -32,9 +42,8 @@ def read_message(message_id):
     return message_dict
 
 @app.route('/filter_by', methods=['GET'])
-def filter_all():
-    client = Client(TWILIO_ACCOUNT_SID, TWILIO_AUTH_TOKEN)
-    messages = client.messages.list(limit=100)
+def view_all_messages():
+    messages = CLIENT.messages.list(limit=100)
     filtered_dict = {}
     for message in messages:
         filtered_dict['message_sid'] = message.sid
@@ -44,9 +53,8 @@ def filter_all():
     return filtered_dict
 
 @app.route('/filter_by/sent/<phone_number>', methods=['GET'])
-def filter_sent_to(phone_number):
-    client = Client(TWILIO_ACCOUNT_SID, TWILIO_AUTH_TOKEN)
-    messages = client.messages.list(limit=100)
+def filter_messages_by_sent_to(phone_number):
+    messages = CLIENT.messages.list(limit=100)
     phone_number = '+1' + str(phone_number)
     filtered_dict = {}
     for message in messages:
@@ -57,9 +65,8 @@ def filter_sent_to(phone_number):
     return filtered_dict
 
 @app.route('/filter_by/from/<phone_number>', methods=['GET'])
-def filter_from(phone_number):
-    client = Client(TWILIO_ACCOUNT_SID, TWILIO_AUTH_TOKEN)
-    messages = client.messages.list(limit=100)
+def filter_messages_by_from(phone_number):
+    messages = CLIENT.messages.list(limit=100)
     phone_number = '+1' + str(phone_number)
     filtered_dict = {}
     for message in messages:
@@ -68,3 +75,5 @@ def filter_from(phone_number):
             filtered_dict['message_sent_from'] = message.from_
             filtered_dict['message_body'] = message.body
     return filtered_dict
+
+# TODO add error handling to check if vaild phone number for /messages and /filter_by urls
